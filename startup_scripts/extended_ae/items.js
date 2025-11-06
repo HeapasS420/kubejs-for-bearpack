c98d3cfb97535e8897521383b570758f0361a3f6
HeapasS420 <dogemanno9@gmail.com>
2025-11-06 16:36:11 +1100

    Update items.js

--- startup_scripts/extended_ae/items.js
+++ startup_scripts/extended_ae/items.js
@@ -1,7 +1,8 @@
 function registerExtendedAEItems(event) {
 
+  const t = type.split(":");
+
   global.EXTENDED_AE_FLUID_CELLS.forEach(type => {
-    const t = type.split(":");
     event.create(`${t[1]}_cell`, 'custom_infinity_cell')
       .texture(`kubejs:item/${t[1]}_cell`)
       .itemType(`${t[0]}:${t[1]}`)
@@ -9,7 +10,6 @@ function registerExtendedAEItems(event) {
   })
 
   global.EXTENDED_AE_ITEM_CELLS.forEach(type => {
-    const t = type.split(":");
     event.create(`${t[1]}_cell`, 'custom_infinity_cell')
       .texture(`kubejs:item/${t[1]}_cell`)
       .itemType(`${t[0]}:${t[1]}`)
